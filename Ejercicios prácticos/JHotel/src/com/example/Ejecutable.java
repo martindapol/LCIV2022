e1a4144f2fe0ff0db409f47f979d4baab2a84554
martindapol <martindapol@gmail.com>
2022-09-01 13:57:51 -0300

    caso practico modelo parcial

---
+++ Ejercicios prácticos/JHotel/src/com/example/Ejecutable.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.example;
+
+import com.example.views.JFPrincipal;
+
+/**
+ *
+ * @author Martin
+ */
+public class Ejecutable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            new JFPrincipal().setVisible(true);
+        } catch (Exception ex) {
+            // handle the error driver
+        }
+    }
+
+}
