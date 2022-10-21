eac4e493f57621e1daf98bd3504f0b1b887b5128
unknown <martindapol@gmail.com>
2022-10-21 09:28:49 -0300

    CRUD productos

---
+++ Ejercicios prácticos/productosCrud/productosCrud/src/main/java/com/example/productosCrud/ProductosCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.productosCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosCrudApplication.class, args);
+	}
+
+}
---
+++ Ejercicios prácticos/productosCrud/productosCrud/src/test/java/com/example/productosCrud/ProductosCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productosCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
