48a946d5006955be331c799a594a9457c6f7f4f4
unknown <martindapol@gmail.com>
2022-11-11 10:13:37 -0300

    Proyecto vacío para prueba de TP

---
+++ Ejercicios prácticos/similtp/src/main/java/com/example/similtp/SimiltpApplication.java
@@ -0,0 +1,13 @@
+package com.example.similtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimiltpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimiltpApplication.class, args);
+	}
+
+}
---
+++ Ejercicios prácticos/similtp/src/test/java/com/example/similtp/SimiltpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.similtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimiltpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
